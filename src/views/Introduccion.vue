1773e3bfbb05dc7737fc52fe989938281a2eb5a6
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-24 12:53:29 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,7 +23,9 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)          
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/3jLg-qgzr0I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          figcaption Video: Comercialización de productos
+            
              
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -130,7 +130,8 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)                
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/TF3vsqXZPJA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; pictur+e-in-picture" allowfullscreen)  
+          figcaption Video: La demanda
 </template>
 
 <script>
